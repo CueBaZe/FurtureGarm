986da215c1933da63acfe4de91aa6516dda2c475
CueBaZe <olivervalente@icloud.com>
2025-04-09 08:19:29 +0200

    Made html and css to login page

---
+++ website/resources/views/login.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card text-center col-10 col-sm-10 col-md-8 col-lg-6">
+                <div class="brand">
+                    <i class='bx bx-time' id="logo"></i>
+                    <h3 class="title">FutureGram</h3>
+                    <p class="motto">Because some messages are meant for later.</p>
+                </div>
+                <form action="">
+                    <div class="inputs">
+                        <div class="emailbox">
+                            <i class='bx bx-user' ></i>
+                            <input type="text" class="email col-8" name="email" placeholder="Email">
+                        </div>
+
+                        <div class="passwordbox">
+                            <i class='bx bxs-hide' ></i>
+                            <input type="text" class="password col-8" name="password" placeholder="Password">
+                        </div>
+                    </div>
+
+                    <input type="submit" name="login" class="loginbtn col-5" value="Login">
+
+                    <div class="rememberMe">
+                        <input type="checkbox" name="rem" class="rememberbox">
+                        <label for="rem">Remember me for 30 days</label>
+                    </div>
+
+                    <div class="link-to-signup">
+                        <p>Don't have an account? <br> <span class="createacc">Create account</span></p> 
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
