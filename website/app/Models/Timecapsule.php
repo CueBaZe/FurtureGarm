de70506f1311df059298bee121a9bc323f74f044
CueBaZe <olivervalente@icloud.com>
2025-04-25 12:09:47 +0200

    Added a new package named MediaLibarary

--- website/app/Models/Timecapsule.php
+++ website/app/Models/Timecapsule.php
@@ -2,8 +2,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Timecapsule extends Model
+class Timecapsule extends Model implements HasMedia
 {
     protected $fillable = ['user_id', 'name', 'text', 'time', 'madeBy'];
 }
