3088d6142d2de2f70a0f4c681bb94427b05b9a30
CueBaZe <olivervalente@icloud.com>
2025-04-29 13:10:43 +0200

    Fixed a typo in create media table

--- website/database/migrations/0001_01_01_000003_create_media_table.php
+++ website/database/migrations/0001_01_01_000003_create_media_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('medias', function (Blueprint $table) {
             $table->id();
-            $table->int('capsule_id');
+            $table->integer('capsule_id');
             $table->string('name');
             $table->string('path');
         }); 
